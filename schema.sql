5d749d38d75b2c4d5705c147be6a64605739d693
Henry <henryboey39@gmail.com>
2025-06-24 21:40:43 +0800

    Updated Schema

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ USE Project;
 
 -- Category Translation
 CREATE TABLE Category_Translation (
-    product_category         VARCHAR(255) PRIMARY KEY,
+    product_category            VARCHAR(255) PRIMARY KEY,
     product_category_translation VARCHAR(255)
 );
 
@@ -22,6 +22,12 @@ CREATE TABLE Product (
     product_height_cm       FLOAT,
     product_width_cm        FLOAT,
     product_photo           VARCHAR(255),
+    has_stock               TINYINT(1),
+    product_description     VARCHAR(255),
+    product_model           VARCHAR(50),
+    product_price           DOUBLE,
+    seller_id               VARCHAR(255),
+    FOREIGN KEY (seller_id) REFERENCES Seller(seller_id),
     FOREIGN KEY (product_category) REFERENCES Category_Translation(product_category)
 );
 
@@ -51,7 +57,12 @@ CREATE TABLE Customer (
     customer_id             VARCHAR(255) PRIMARY KEY,
     customer_zip_code       VARCHAR(20),
     username                VARCHAR(255),
-    password_hash           VARCHAR(255)
+    password_hash           VARCHAR(255),
+    name                    VARCHAR(100),
+    contact                 VARCHAR(20),
+    email                   VARCHAR(100),
+    address                 VARCHAR(100),
+    photo                   VARCHAR(255)
 );
 
 -- Order
@@ -64,6 +75,9 @@ CREATE TABLE Orders(
     order_delivery_carrier_date     DATETIME,
     order_delivery_customer_date    DATETIME,
     order_estimated_delivery_date   DATETIME,
+    shipping_address               VARCHAR(255),
+    shipping_postal_code            VARCHAR(20),
+    order_cancellation_reason       VARCHAR(255),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
