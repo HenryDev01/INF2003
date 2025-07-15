afe4283257a304f8f2faaaac493573b648846c5f
Henry <henryboey39@gmail.com>
2025-07-15 18:43:06 +0800

    Added get dimenions methods

--- App/Models/Inventory.py
+++ App/Models/Inventory.py
@@ -1,8 +1,9 @@
-# TODO: The current models are not tailored for this project. Please modify them to suit your specific requirements.
-
 class Product:
 
-    def __init__(self, id, product_category, product_name, product_weight_g, product_length_cm,product_height_cm, product_width_cm, product_photo, has_stock, product_description, product_model,product_price, seller_id):
+    def __init__(self, id, product_category, product_name, product_weight_g,
+                 product_length_cm, product_height_cm, product_width_cm,
+                 product_photo, has_stock, product_description,
+                 product_model, product_price, seller_id):
         self.__product_id = id
         self.__category = product_category
         self.__name = product_name
@@ -18,18 +19,18 @@ class Product:
         self.__ratings = 0
         self.__seller_id = seller_id
 
-    # Getter and setter for product_id
+    # product_id
     def get_product_id(self):
         return self.__product_id
 
-
+    # Category
     def set_category(self, category):
         self.__category = category
 
     def get_category(self):
         return self.__category
 
-    # URL
+    # Photo
     def set_photo(self, url):
         self.__photo = url
 
@@ -78,8 +79,41 @@ class Product:
     def get_model(self):
         return self.__model
 
+    # Rating
     def set_rating(self, ratings):
         self.__ratings = ratings
 
     def get_rating(self):
         return self.__ratings
+
+    # Weight
+    def set_weight(self, weight):
+        self.__weight = weight
+
+    def get_weight(self):
+        return self.__weight
+
+    # Length
+    def set_length(self, length):
+        self.__length = length
+
+    def get_length(self):
+        return self.__length
+
+    # Height
+    def set_height(self, height):
+        self.__height = height
+
+    def get_height(self):
+        return self.__height
+
+    # Width
+    def set_width(self, width):
+        self.__width = width
+
+    def get_width(self):
+        return self.__width
+
+    # Seller ID
+    def get_seller_id(self):
+        return self.__seller_id
